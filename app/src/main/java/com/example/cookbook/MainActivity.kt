8782dd02de7e0a5f9eb639f79d129b8b793437c0
andreistaravoitau <andrei.v.staravoitau@gmail.com>
2023-04-30 16:30:54 +0200

    Add tabs

--- app/src/main/java/com/example/cookbook/MainActivity.kt
+++ app/src/main/java/com/example/cookbook/MainActivity.kt
@@ -1,11 +1,82 @@
 package com.example.cookbook
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.BlendModeColorFilterCompat
+import androidx.core.graphics.BlendModeCompat
+import com.example.cookbook.databinding.ActivityDetailBinding
+import com.example.cookbook.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // Tabs Customization
+        binding.tabLayout.setSelectedTabIndicatorColor(Color.WHITE)
+        binding.tabLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.purple_200))
+        binding.tabLayout.tabTextColors = ContextCompat.getColorStateList(this, android.R.color.white)
+
+        // Set different Text Color for Tabs for when are selected or not
+        //binding.tabLayout.setTabTextColors(R.color.normalTabTextColor, R.color.selectedTabTextColor)
+
+        // Number Of Tabs
+        val numberOfTabs = 3
+
+        // Set Tabs in the center
+        //binding.tabLayout.tabGravity = TabLayout.GRAVITY_CENTER
+
+        // Show all Tabs in screen
+        binding.tabLayout.tabMode = TabLayout.MODE_FIXED
+
+        // Scroll to see all Tabs
+        //binding.tabLayout.tabMode = TabLayout.MODE_SCROLLABLE
+
+        // Set Tab icons next to the text, instead of above the text
+        binding.tabLayout.isInlineLabel = true
+
+        // Set the ViewPager Adapter
+        val adapter = TabsPagerAdapter(supportFragmentManager, lifecycle, numberOfTabs)
+        binding.tabsViewpager.adapter = adapter
+
+        // Enable Swipe
+        binding.tabsViewpager.isUserInputEnabled = true
+
+        // Link the TabLayout and the ViewPager2 together and Set Text & Icons
+        TabLayoutMediator(binding.tabLayout, binding.tabsViewpager) { tab, position ->
+            when (position) {
+                0 -> {
+                    tab.text = "Home"
+                    tab.setIcon(R.drawable.ic_home)
+                }
+                1 -> {
+                    tab.text = "Breakfasts"
+                    tab.setIcon(R.drawable.ic_breakfast)
+
+                }
+                2 -> {
+                    tab.text = "Fast food"
+                    tab.setIcon(R.drawable.ic_fastfood)
+                }
+
+            }
+            // Change color of the icons
+            tab.icon?.colorFilter =
+                BlendModeColorFilterCompat.createBlendModeColorFilterCompat(
+                    Color.WHITE,
+                    BlendModeCompat.SRC_ATOP
+                )
+        }.attach()
     }
 }
