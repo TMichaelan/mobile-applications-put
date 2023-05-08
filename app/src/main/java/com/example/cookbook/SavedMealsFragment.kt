e2aafef48400c055d3fbd380fd7b9db745f4f402
TMichael <bigapplematthias@gmail.com>
2023-05-08 22:56:57 +0200

    add favorites + db

--- app/src/main/java/com/example/cookbook/SavedMealsFragment.kt
+++ app/src/main/java/com/example/cookbook/SavedMealsFragment.kt
@@ -1,55 +1,59 @@
 package com.example.cookbook
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cookbook.db.AppDatabase
+import com.example.cookbook.models.Meal
+import kotlinx.coroutines.launch
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SavedMealsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class SavedMealsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class SavedMealsFragment : Fragment(), MealAdapter.OnItemClickListener {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_meals_grid, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView = view.findViewById(R.id.recycleView)
+        recyclerView.layoutManager = GridLayoutManager(context, 2)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // Загрузка сохраненных блюд из базы данных и отображение их с помощью RecyclerView
+        loadSavedMeals()
+    }
+
+    private fun loadSavedMeals() {
+        val db = AppDatabase.getInstance(requireContext())
+        lifecycleScope.launch {
+            val savedMeals = db.mealDao().getAllMeals()
+            val mealAdapter = MealAdapter(ArrayList(savedMeals), this@SavedMealsFragment)
+            recyclerView.adapter = mealAdapter
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_saved_meals, container, false)
+    override fun onItemClick(meal: Meal) {
+        // Здесь вы можете открыть MealDetailActivity и передать информацию о выбранном блюде
+        val intent = Intent(requireContext(), DetailActivity::class.java)
+        intent.putExtra("meal", meal)
+        startActivity(intent)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SavedMealsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                SavedMealsFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+        @JvmStatic
+        fun newInstance() = SavedMealsFragment()
     }
 }
