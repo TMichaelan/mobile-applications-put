68f356fb24f3a5f0368fda55a863b89b8fa4adf3
andreistaravoitau <andrei.v.staravoitau@gmail.com>
2023-05-09 00:39:55 +0200

    Add tablet optimization

--- app/src/main/java/com/example/cookbook/MealsGridFragment.kt
+++ app/src/main/java/com/example/cookbook/MealsGridFragment.kt
@@ -3,10 +3,13 @@ package com.example.cookbook
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.isGone
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cookbook.data.retrofit.RetrofitInstance
@@ -30,14 +33,20 @@ class MealsGridFragment : Fragment(), MealAdapter.OnItemClickListener {
 ////            .commit()
 //
 //    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        val layoutId = if (resources.getBoolean(R.bool.is_tablet)) R.layout.meals_grid_and_detail else R.layout.fragment_meals_grid
+        return inflater.inflate(layoutId, container, false)
+//        if (tag == "mobile"){
+//            return inflater.inflate(R.layout.fragment_meals_grid, container, false)
+//        }
+//        else{
+//            return inflater.inflate(R.layout.meals_grid_and_detail, container, false)
+//        }
 
-        return inflater.inflate(R.layout.fragment_meals_grid, container, false)
     }
 
     private fun getRandomMeal(callback: (ArrayList<Meal>) -> Unit) {
@@ -72,6 +81,7 @@ class MealsGridFragment : Fragment(), MealAdapter.OnItemClickListener {
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
         super.onViewCreated(view, savedInstanceState)
         // getting the mealList
         getRandomMeal{mealList ->
@@ -80,7 +90,9 @@ class MealsGridFragment : Fragment(), MealAdapter.OnItemClickListener {
             // Set the LayoutManager that
             // this RecyclerView will use.
             val recyclerView: RecyclerView =view.findViewById(R.id.recycleView)
-            recyclerView.layoutManager = GridLayoutManager(context, 2)
+            var columns = 2
+            if (resources.getBoolean(R.bool.is_tablet)) columns = 1
+            recyclerView.layoutManager = GridLayoutManager(context, columns)
             // adapter instance is set to the
             // recyclerview to inflate the items.
             recyclerView.adapter = itemAdapter
@@ -90,9 +102,22 @@ class MealsGridFragment : Fragment(), MealAdapter.OnItemClickListener {
 
     override fun onItemClick(meal: Meal) {
         // Здесь вы можете открыть MealDetailActivity и передать информацию о выбранном блюде
-        val intent = Intent(requireContext(), DetailActivity::class.java)
-        intent.putExtra("meal", meal)
-        startActivity(intent)
+        if (resources.getBoolean(R.bool.is_tablet)) {
+            val text = view?.findViewById<TextView>(R.id.textView)
+            if (text != null) {
+                text.visibility = View.GONE
+            }
+
+            val mealDetailFragment = MealDetailFragment.newInstance(meal)
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.detail_frame, mealDetailFragment)
+                .commit()
+        }
+        else{
+            val intent = Intent(requireContext(), DetailActivity::class.java)
+            intent.putExtra("meal", meal)
+            startActivity(intent)
+        }
     }
 
     companion object {
