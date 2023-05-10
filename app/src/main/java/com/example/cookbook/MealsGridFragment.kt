d7112fb7333b4ab01a23f12406ea6d5c06c77bd3
TMichael <bigapplematthias@gmail.com>
2023-05-10 19:55:37 +0200

    rotate updating meals fix

--- app/src/main/java/com/example/cookbook/MealsGridFragment.kt
+++ app/src/main/java/com/example/cookbook/MealsGridFragment.kt
@@ -31,39 +31,12 @@ class MealsGridFragment : Fragment(), MealAdapter.OnItemClickListener {
         val layoutId = if (resources.getBoolean(R.bool.is_tablet)) R.layout.meals_grid_and_detail else R.layout.fragment_meals_grid
         return inflater.inflate(layoutId, container)
     }
-//    private fun getRandomMeal(callback: (ArrayList<Meal>) -> Unit) {
-//        val mealList = ArrayList<Meal>()
-//        var count = 0
-//        val randomMealAmount = 20
-//        for (i in 1..randomMealAmount) {
-//            RetrofitInstance.mealAPI.getRandomMeal().enqueue(object : retrofit2.Callback<MealList> {
-//                override fun onResponse(call: Call<MealList>, response: Response<MealList>) {
-//                    if (response.body() != null) {
-//                        val randomMeal: Meal = response.body()!!.meals[0]
-//                        mealList += randomMeal
-////                        Log.d("MyLog", "meal ${randomMeal.idMeal} name ${randomMeal.strMeal} mealList${mealList}")
-//                    }
-//                    count++
-//                    if (count == randomMealAmount) {
-//                        callback(mealList)
-//                    }
-//                }
-//
-//                override fun onFailure(call: Call<MealList>, t: Throwable) {
-////                    Log.d("MyLog", "error: ${t.message.toString()}")
-//                    count++
-//                    if (count == randomMealAmount) {
-//                        callback(mealList)
-//                    }
-//                }
-//            })
-//        }
-//    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // Получение списка блюд из аргументов
+        val mealList = arguments?.getSerializable("mealList") as? ArrayList<Meal>
 
-        // getting the mealList
-        MealRepository.getRandomMeal { mealList ->
+        if (mealList != null) {
             // Assign mealList to ItemAdapter
             val itemAdapter = MealAdapter(mealList, this)
             // Set the LayoutManager that
@@ -116,12 +89,6 @@ class MealsGridFragment : Fragment(), MealAdapter.OnItemClickListener {
             startActivity(intent)
         }
     }
-
-//    companion object {
-//        @JvmStatic
-//        fun newInstance() = MealsGridFragment()
-//    }
-
     companion object {
         @JvmStatic
         fun newInstance(mealList: ArrayList<Meal>) = MealsGridFragment().apply {
